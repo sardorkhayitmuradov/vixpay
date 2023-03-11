fed66feb4741d362d70b5330707a477d82923107
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-03-11 18:21:19 +0500

    bugs fixed

--- components/header.js
+++ components/header.js
@@ -36,21 +36,21 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link href={`/` + "#resource"} className={`py-2 text-[#4B5563] hover:text-[#22C9EE] font-PoppinsBold text-[18px] leading-[135%] tracking-[-0.26px] px-3 ${router.pathname == "/resource" ? "bg-[#CFF8FE] hover:text-[#4b5563] rounded-[8px]": ""}`}>
+              <Link href={`http://www.yollet.net/doc/block-api-reference.html`} target="_blank" className={`py-2 text-[#4B5563] hover:text-[#22C9EE] font-PoppinsBold text-[18px] leading-[135%] tracking-[-0.26px] px-3 ${router.pathname == "/resource" ? "bg-[#CFF8FE] hover:text-[#4b5563] rounded-[8px]": ""}`}>
               Resource
               </Link>
             </li>
-            {/* <li>
-              <Link href="/scan" className={`py-2 text-[#22C9EE] font-PoppinsBold text-[18px] leading-[135%] tracking-[-0.26px] px-3 ${router.pathname == "/scan" ? "bg-[#CFF8FE] hover:text-[#4b5563] rounded-[8px]": ""}`}>
+            <li>
+              <Link href="https://yollet-scan.vercel.app/" target="_blank" className={`py-2 text-[#22C9EE] font-PoppinsBold text-[18px] leading-[135%] tracking-[-0.26px] px-3 ${router.pathname == "/scan" ? "bg-[#CFF8FE] hover:text-[#4b5563] rounded-[8px]": ""}`}>
               VIXCOScan
               </Link>
-            </li> */}
+            </li>
             <li>
               <Link href={`/` + "#services"} className={`py-2 text-[#4B5563] hover:text-[#22C9EE] font-PoppinsBold text-[18px] leading-[135%] tracking-[-0.26px] px-3 ${router.pathname == "/services" ? "bg-[#CFF8FE] hover:text-[#4b5563] rounded-[8px]": ""}`}>
               Partners Service
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link href={`/write`} className={`py-2 text-[#4B5563] hover:text-[#22C9EE] font-PoppinsBold text-[18px] leading-[135%] tracking-[-0.26px] px-3 ${router.pathname == "/write" ? "bg-[#CFF8FE] hover:text-[#4b5563] rounded-[8px]": ""}`}>
             Write
               </Link>
@@ -64,7 +64,7 @@ const Header = () => {
               <Link href={`/read`} className={`py-2 text-[#4B5563] hover:text-[#22C9EE] font-PoppinsBold text-[18px] leading-[135%] tracking-[-0.26px] px-3 ${router.pathname == "/read" ? "bg-[#CFF8FE] hover:text-[#4b5563] rounded-[8px]": ""}`}>
             Read
               </Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
       </div>
